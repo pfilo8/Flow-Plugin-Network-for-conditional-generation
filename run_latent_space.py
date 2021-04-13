825529aadb7db66621f9f8bbc89687f0ef55beb7
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2021-04-14 00:28:05 +0200

    Latent space script fix.

--- run_latent_space.py
+++ run_latent_space.py
@@ -29,9 +29,8 @@ def get_latent_space(model, dataloader):
 
 args = get_parser_latent_space().parse_args()
 path = args.path
-base_path = Path(path).parent.parent
 
-data_dir = base_path / Path('latent_space')
+data_dir = path / Path('latent_space')
 data_dir.mkdir(exist_ok=True)
 
 model = load_model(path, 'vae').to(DEVICE)
@@ -52,13 +51,17 @@ dataset_valid = CelebA(root='data', split="valid", transform=transform, download
 dataset_test = CelebA(root='data', split="test", transform=transform, download=False)
 
 dataloader_train = DataLoader(dataset_train, batch_size=bs)
+dataloader_valid = DataLoader(dataset_valid, batch_size=bs)
 dataloader_test = DataLoader(dataset_test, batch_size=bs)
 
 zs_train, ys_train = get_latent_space(model, dataloader_train)
+zs_valid, ys_valid = get_latent_space(model, dataloader_valid)
 zs_test, ys_test = get_latent_space(model, dataloader_test)
 
 pd.DataFrame(zs_train).to_csv(data_dir / Path('z_train.csv'), index=False)
+pd.DataFrame(zs_valid).to_csv(data_dir / Path('z_valid.csv'), index=False)
 pd.DataFrame(zs_test).to_csv(data_dir / Path('z_test.csv'), index=False)
 
 pd.DataFrame(ys_train).to_csv(data_dir / Path('y_train.csv'), index=False)
+pd.DataFrame(ys_valid).to_csv(data_dir / Path('y_valid.csv'), index=False)
 pd.DataFrame(ys_test).to_csv(data_dir / Path('y_test.csv'), index=False)
