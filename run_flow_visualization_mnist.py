ef1c7fff5103cb8fa0364ad400d1860f17afd1b7
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2021-05-10 12:23:11 +0200

    MNIST VAE without data scaling.

---
+++ run_flow_visualization_mnist.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import torch
+from torchvision.utils import make_grid
+
+from utils import get_parser_flow_visualization
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def show_image(image, filepath):
+    plt.figure(figsize=(16, 8))
+    plt.imshow(image.squeeze(0).permute(1, 2, 0).detach().cpu())
+    plt.axis('off')
+    plt.savefig(filepath)
+
+
+args = get_parser_flow_visualization().parse_args()
+save_path = args.flow_path / Path('media')
+
+flow = torch.load(args.flow_path / Path('checkpoints/model.pkt')).to(DEVICE)
+model = torch.load(args.model_path / Path('checkpoints/model.pkt')).to(DEVICE)
+
+n_samples = 64
+n_row = 8
+
+outputs = []
+
+for i in range(10):
+    context = torch.zeros(1, 10).to(DEVICE)
+    context[0][i] = 1.0
+    print(context)
+
+    samples = flow.sample(n_samples, context).squeeze(0)
+    output = model.decode(samples)
+    outputs.append(output)
+    show_image(
+        (make_grid(output, nrow=n_row)),
+        filepath=save_path / Path(f"{i}.png")
+    )
+
+show_image(
+    make_grid(torch.cat(outputs)[::4], 16),
+    filepath=save_path / Path("all-16.png")
+)
+
+show_image(
+    make_grid(torch.cat(outputs)[::2], 32),
+    filepath=save_path / Path("all-32.png")
+)
--- utils.py
+++ utils.py
@@ -35,6 +35,23 @@ def get_parser_latent_space():
     return parser
 
 
+def get_parser_flow_visualization():
+    parser = argparse.ArgumentParser(
+        description='Visualization of the conditional generation of the flow for MNIST Experiment.'
+    )
+    parser.add_argument(
+        '--model_path',
+        metavar='FILE',
+        help='Model path'
+    )
+    parser.add_argument(
+        '--flow_path',
+        metavar='FILE',
+        help='Flow path'
+    )
+    return parser
+
+
 def get_config(filename):
     with open(filename, 'r') as file:
         try:
