871b591067ec3e27cb742a44af5684d08cb0596d
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2021-05-24 17:13:19 +0200

    CelebA scripts update.

--- run_flow_visualization_celeba.py
+++ run_flow_visualization_celeba.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from uuid import uuid4
 
 import matplotlib.pyplot as plt
 import torch
@@ -7,13 +8,22 @@ from torchvision.utils import make_grid
 from models.vae.msp import MSP
 from utils import get_parser_model_flow
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+CLASSES = [
+    '5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose',
+    'Black_Hair', 'Blond_Hair', 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses',
+    'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes',
+    'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns', 'Smiling',
+    'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace',
+    'Wearing_Necktie', 'Young'
+]
+plt.ioff()
 
 
 def show_image(image, filepath):
-    plt.figure(figsize=(16, 8))
     plt.imshow(image.squeeze(0).permute(1, 2, 0).detach().cpu())
     plt.axis('off')
+    plt.tight_layout()
     plt.savefig(filepath)
 
 
@@ -26,39 +36,21 @@ model.load_state_dict(torch.load(args.model_path / Path('checkpoints/MSP_CelebA.
 model.to(DEVICE)
 model.eval()
 
-n_samples = 64
-n_row = 8
-
-classes = [
-    '5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose',
-    'Black_Hair', 'Blond_Hair', 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses',
-    'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes',
-    'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns', 'Smiling',
-    'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace',
-    'Wearing_Necktie', 'Young'
-]
+n_samples = 1
+n_row = 1
 
 outputs = []
 
-for idx, label in enumerate(classes):
+for idx, label in enumerate(CLASSES):
     context = torch.zeros(1, 40).to(DEVICE)
     context[0][idx] = 1.0
     print(context)
 
     samples = flow.sample(n_samples, context).squeeze(0)
-    output = model.decode(samples)
-    outputs.append(output)
+    output = model.decoder(samples)
+    output = output.add_(1.0).div_(2.0)
+    # outputs.append(output)
     show_image(
-        (make_grid(output, nrow=n_row)),
-        filepath=save_path / Path(f"{label}.png")
+        (make_grid(output, nrow=n_row, padding=0)),
+        filepath=save_path / Path(f"{label}-{uuid4()}.png")
     )
-
-show_image(
-    make_grid(torch.cat(outputs)[::4], 16),
-    filepath=save_path / Path("all-16.png")
-)
-
-show_image(
-    make_grid(torch.cat(outputs)[::2], 32),
-    filepath=save_path / Path("all-32.png")
-)
--- run_latent_space_celeba.py
+++ run_latent_space_celeba.py
@@ -14,6 +14,14 @@ from utils import get_parser_model_flow
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+CLASSES = [
+    '5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose',
+    'Black_Hair', 'Blond_Hair', 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses',
+    'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes',
+    'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns', 'Smiling',
+    'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace',
+    'Wearing_Necktie', 'Young'
+]
 
 def get_latent_space(model, dataloader):
     ys = []
@@ -64,6 +72,6 @@ pd.DataFrame(zs_train).to_csv(data_dir / Path('z_train.csv'), index=False)
 pd.DataFrame(zs_valid).to_csv(data_dir / Path('z_valid.csv'), index=False)
 pd.DataFrame(zs_test).to_csv(data_dir / Path('z_test.csv'), index=False)
 
-pd.DataFrame(ys_train).to_csv(data_dir / Path('y_train.csv'), index=False)
-pd.DataFrame(ys_valid).to_csv(data_dir / Path('y_valid.csv'), index=False)
-pd.DataFrame(ys_test).to_csv(data_dir / Path('y_test.csv'), index=False)
+pd.DataFrame(ys_train, columns=CLASSES).to_csv(data_dir / Path('y_train.csv'), index=False)
+pd.DataFrame(ys_valid, columns=CLASSES).to_csv(data_dir / Path('y_valid.csv'), index=False)
+pd.DataFrame(ys_test, columns=CLASSES).to_csv(data_dir / Path('y_test.csv'), index=False)
